d21754361b8b244d7062e85dd7cc802243b6fd2a
FJeffersonferia <126828472+FJeffersonferia@users.noreply.github.com>
2024-05-04 14:33:54 -0500

    Add files via upload

---
+++ Hello_word.js
@@ -0,0 +1,57 @@
+// String
+var bootcamp = 'Henry';
+
+// Números
+var numeroEntero = 17;
+var numeroNegativo = -4;
+var numeroDecimal = 11.75;
+
+// Booleanos
+var estoyEntendiendo = true;
+var estoyAburrido = false;
+
+// Undefined
+var cajon1;
+console.log(cajon1);
+
+// Null
+var sinNada = null;
+console.log("Hello word");
+//Suma
+console.log(3 + 5);
+
+//Resta
+console.log(14 - 7);
+
+//Multiplicacion
+console.log(8 * 6);
+
+//Division
+console.log(30 / 9);
+
+//Resto
+console.log(95 % 4);
+
+//Prueba de precedencia
+console.log(3 + 5 * 2 - 8);
+
+function viajar(destino) {
+    if(destino === 'Brasil') {
+             console.log('Gire a la izquierda');
+    } else if(destino === 'Argentina') {
+             console.log('Gire a la derecha');
+    } else {
+             console.log('Nos perdimos');''
+    }
+  };
+  viajar('Brasil');
+  viajar('Argentina');
+
+  function puedeManejar(edad) {
+    if(edad >= 18) {
+             console.log( true );
+    } 
+    console.log( false );
+  }
+  
+  puedeManejar(17);
